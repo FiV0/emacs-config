ca92ae1d98611eb008bad45bc02b1b90e9bdcc43
FiV0 <finn.volkel@gmail.com>
2021-07-31 11:28:21 +0200

    babashka-jack-in

--- custom-clj.el
+++ custom-clj.el
@@ -97,5 +97,63 @@
 
 (require 'cider-eval-sexp-fu)
 
+;; babashka
+(defun babashka-quit ()
+  (interactive)
+  (when (get-buffer "*babashka-nrepl-server*")
+    (kill-buffer "*babashka-nrepl-server*"))
+  (when (get-buffer "*babashka-repl*")
+    (kill-buffer "*babashka-repl*")))
+
+(defun babashka-jack-in (&optional connected-callback)
+  (interactive)
+  (babashka-quit)
+  (let* ((cmd "bb --nrepl-server 0")
+         (serv-buf (get-buffer-create "*babashka-nrepl-server*"))
+         (host "127.0.0.1")
+         (repl-builder (lambda (port)
+                         (lambda (_)
+                           (let ((repl-buf (get-buffer-create "*babashka-repl*")))
+                             (with-current-buffer repl-buf
+                               (cider-repl-create (list :repl-buffer repl-buf
+                                                        :repl-type 'clj
+                                                        :host host
+                                                        :port port
+                                                        :project-dir "~"
+                                                        :session-name "babashka"
+                                                        :repl-init-function (lambda ()
+                                                                              (setq-local cljr-suppress-no-project-warning t
+                                                                                          cljr-suppress-middleware-warnings t)
+                                                                              (rename-buffer "*babashka-repl*")))))))))
+         (port-filter (lambda (serv-buf)
+                        (print (boundp serv-buf))
+                        (lambda (process output)
+                          (print (boundp serv-buf))
+                          (when (buffer-live-p serv-buf)
+                            (print (boundp serv-buf))
+                            (with-current-buffer serv-buf
+                              (insert output)
+                              (when (string-match "Started nREPL server at 127.0.0.1:\\([0-9]+\\)" output)
+                                (let ((port (string-to-number (match-string 1 output))))
+                                  (setq nrepl-endpoint (list :host host :port port))
+                                  (let ((client-proc (nrepl-start-client-process
+                                                      host
+                                                      port
+                                                      process
+                                                      (funcall repl-builder port))))
+                                    (set-process-query-on-exit-flag client-proc nil)
+                                    (when connected-callback
+                                      (funcall connected-callback client-proc)))))))))))
+    (with-current-buffer serv-buf
+      (setq nrepl-is-server t
+            nrepl-server-command cmd))
+    (print (boundp serv-buf))
+    (let ((serv-proc (start-file-process-shell-command "babashka-nrepl-server" serv-buf cmd)))
+      (set-process-query-on-exit-flag serv-proc nil)
+      (set-process-filter serv-proc (funcall port-filter serv-buf))
+      (set-process-sentinel serv-proc 'nrepl-server-sentinel)
+      (set-process-coding-system serv-proc 'utf-8-unix 'utf-8-unix)))
+  nil)
+
 (provide 'custom-clj)
 ;;; custom-clj.el ends here
