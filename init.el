79414312cc501f7b57f33011b05e6cd6eee3158f
FiV0 <finn.volkel@gmail.com>
2021-07-31 15:04:06 +0200

    Set org-roam-v2-ack before it loads

--- init.el
+++ init.el
@@ -256,10 +256,10 @@
 (add-hook 'scheme-mode-hook                      #'rainbow-delimiters-mode)
 
 ;; org roam
+(setq org-roam-v2-ack t)
 (require 'org-roam)
 (setq org-roam-directory "~/Code/org-roam/")
 (add-hook 'after-init-hook 'org-roam-mode)
-(setq org-roam-v2-ack t)
 
 (with-eval-after-load 'org-roam
   (define-key org-roam-mode-map (kbd "C-c n l") 'org-roam)
