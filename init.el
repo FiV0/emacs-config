e27320bb4023ccefee07366418737f81764b3428
FiVo <finn.volkel@gmail.com>
2023-06-18 18:27:24 +0200

    Some projectile intializaiton changes

--- init.el
+++ init.el
@@ -102,10 +102,12 @@
 
 ;; projectile
 (require 'projectile)
-(projectile-mode +1)
+(projectile-global-mode)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (setq projectile-indexing-method 'hybrid) ;; indexes non tracked files
+(setq projectile-sort-order 'recently-active)
+(setq projectile-completion-system 'helm)
 
 ;; to disable weird helm completion error
 ;; see non defined function here
